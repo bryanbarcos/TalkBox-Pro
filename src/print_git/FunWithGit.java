3bf0d3d834f023746df937f0517e4d80562441ad
bryanbarcos <bryanbarcos@hotmail.ca>
2019-01-18 16:46:17 -0500

    This is a practice commit, where i made a new practice branch.

--- src/print_git/FunWithGit.java
+++ src/print_git/FunWithGit.java
@@ -5,5 +5,10 @@ public class FunWithGit {
 	public static void main (String args[]) {
 	System.out.println("Lets do this");
 	System.out.println("my name is bryan");
+	System.out.println("this is a practice line for the new branch");
+	
+	int x = 10;
+	int y = 20;
+	System.out.println(x + y);
 	}
 }
